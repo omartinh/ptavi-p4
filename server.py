62d0cae75af637461acc9f396f1b25870d322473
Oscar Martin Herrera <omartin@gamma10.aulas.gsyc.urjc.es>
2016-10-24 10:50:51 +0200

    Pratica 4 ejer4.6

--- server.py
+++ server.py
@@ -5,24 +5,44 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-import client
+import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    def client_address(self):
-        return(client.IP & client.PORT)
+    c_dicc = {}
 
     def handle(self):
-        self.wfile.write(b"Hemos recibido tu peticion") 
-        for line in self.rfile: # rfile lee el fichero
-            print(client_address())
-            print("El cliente nos manda: ", line.decode('utf-8')) # codificamos los bytes, de la linea q leemos
+       
+       
+        self.wfile.write(b"Hemos recibido tu peticion")
+        c_a_list=list(self.client_address)
+        ip = c_a_list[0]
+        port = c_a_list[1]
+        print('IP:', ip)
+        print('PORT:',  port)
+        for line in self.rfile:
+            print("El cliente nos manda: ", line.decode('utf-8'))
+
+        data_list = line.decode('utf-8').split(' ')
+
+        if data_list[0] == 'REGISTER':
+            self.c_dicc[ip]=data_list[1]
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+            
+            
+            
+        
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)  # tupla-->('',6001) y manegador--> EchoHandler trata las peticiones
+    try:
+        PORT = int(sys.argv[1])
+    except ValueError:
+        sys.exit('No integer value')
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
