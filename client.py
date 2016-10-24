7d16717a4f6937168bf9405966ac590c9847fedd
Oscar Martin Herrera <omartin@gamma10.aulas.gsyc.urjc.es>
2016-10-24 10:50:01 +0200

    PRACTICA 4 client.py 4.6

--- client.py
+++ client.py
@@ -4,19 +4,24 @@
 Programa cliente UDP que abre un socket a un servidor
 """
 
-import socket
+import socket # para abrir caminos cliente-servidor
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = sys.argv[3:]
+    # sys coge 
+except:
+    sys.exit('Error : IP, PORT OR LINE')
+
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket: # (tipo red(internet), tipo paquete(UDP))
+    my_socket.connect((IP, PORT)) # se le pasa un valor que es este caso es una tupla
+    print("Enviando:", ' '.join(LINE))
+    my_socket.send(bytes(' '.join(LINE), 'utf-8') + b'\r\n')  # la b es de bytes
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
+    print('Recibido -- ', data.decode('utf-8')) # decode pas de bytes a utf-8
 print("Socket terminado.")
